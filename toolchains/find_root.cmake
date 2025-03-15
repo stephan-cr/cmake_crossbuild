cdf4655fa0bd86c9cf041133ad76b1fa048cc87a
Stephan Creutz <stephan.cr@gmx.de>
2025-03-15 15:35:31 +0100

    consolidate "*FIND_ROOT*" variables in separate file

---
+++ toolchains/find_root.cmake
@@ -0,0 +1,5 @@
+# search for programs in the build host directories
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# for libraries and headers in the target directories
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
--- toolchains/gcc-arm-none-toolchain.cmake
+++ toolchains/gcc-arm-none-toolchain.cmake
@@ -8,8 +8,4 @@ set(CMAKE_EXE_LINKER_FLAGS_INIT "--specs=nosys.specs" CACHE INTERNAL "")
 
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
-# search for programs in the build host directories
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-# for libraries and headers in the target directories
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+include(${CMAKE_CURRENT_LIST_DIR}/find_root.cmake)
