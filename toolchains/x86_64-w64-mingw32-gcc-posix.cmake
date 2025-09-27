193a9e3c8d4dae99502323f968090cc3ab3272b1
Stephan Creutz <stephan.cr@gmx.de>
2025-09-27 20:09:21 +0200

    improve MinGW GCC toolchain

--- toolchains/x86_64-w64-mingw32-gcc-posix.cmake
+++ toolchains/x86_64-w64-mingw32-gcc-posix.cmake
@@ -1,5 +1,13 @@
+# https://www.mingw-w64.org/build-systems/cmake/
+
 set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
 set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
 set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+set(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
+
+# where is the target environment
+set(CMAKE_SYSROOT /usr/x86_64-w64-mingw32)
+
+include(${CMAKE_CURRENT_LIST_DIR}/find_root.cmake)
